a21b2c129eeafeb3fde3faf47ff9efc4565bc97a
MysteryCrystal <106497267+MysteryCrystal@users.noreply.github.com>
2023-11-17 22:46:57 +0000

    first pattern

--- 2203391.py
+++ 2203391.py
@@ -1,5 +1,27 @@
 from graphics import * 
 
+#draws corner patch and returns all objetcs for later use
+def drawCorner(win, point, colour):
+    objects = []
+    y = point[1]
+    bottomLeft = Point(point[0], point[1]+100)
+    for x in range(10):
+        topRight = Point(point[0]+100-x*10, y+x*10)
+        if x%2==0:
+            box = drawBox(win, bottomLeft, topRight, colour)
+        else:
+            box = drawBox(win, bottomLeft, topRight, "white")
+        objects.append(box)
+    return objects
+
+#draws box and returns object
+def drawBox(win, point1, point2, colour):
+    box = Rectangle(point1, point2)
+    box.setFill(colour)
+    box.draw(win)
+    return box
+
+#returns all top left coords in order
 def getCoord(size):
     coords = []
     for y in range(size):
@@ -8,26 +30,39 @@ def getCoord(size):
             coord.append(x*100)
             coord.append(y*100)
             coords.append(coord)
-
     return coords
 
+#returns pattern name based on top left coord
+def getPattern(size):
+    patterns = []
+    for y in range(size):
+        for x in range(size):
+            if x == y:
+                patterns.append("corner")
+            elif y%2==0:
+                patterns.append("tile")
+            else:
+                patterns.append("null")
+    return patterns
+
+#returns colours for all tiles in order
 def getColour(size, choices):
     colours = []
     for y in range(size):
         for x in range(size):
-            if y%2==0:
-                if x%2 == 0:
+            if size-1>y>0 and size-1>x>0:
+                colours.append(choices[2])
+            elif y%2 == 0:
+                if x%2==0:
                     colours.append(choices[0])
                 else:
                     colours.append(choices[1])
-            if y%1==0:
+            else:
                 if x%2==0:
                     colours.append(choices[1])
                 else:
                     colours.append(choices[0])
-            print(x, y, colours[len(colours)-1])
-
-    print(colours)
+    return colours
 
 
 def createWin(name, size):
@@ -66,9 +101,13 @@ def getSizeColour(sizes, colours):
 def main():
     sizes = [5, 7, 9]
     colours = ["red", "green", "blue", "magenta", "orange", "yellow", "cyan"]
+    patterns = ["tile", "corner"]
     #size, choices = getSizeColour(sizes, colours)
-    getColour(5, ["blue", "orange", "red"])
+    #getColour(5, ["blue", "orange", "red"])
+    #getPattern(5)
+    win = createWin("Patchwork", 500)
+    objects = drawCorner(win, [0, 0], "red")
+    win.getMouse()
 
-    
 
 main()
