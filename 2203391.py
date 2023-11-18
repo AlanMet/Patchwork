2761a916e3b3354f6d3af091a5fffab61cbafc6a
MysteryCrystal <106497267+MysteryCrystal@users.noreply.github.com>
2023-11-18 00:22:44 +0000

    Update 2203391.py

--- 2203391.py
+++ 2203391.py
@@ -1,5 +1,21 @@
 from graphics import * 
 
+def drawPatchwork(win, coords, patterns, colours):
+    for x in range(0, len(coords)):
+        colour = colours[x]
+        coord = coords[x]
+        pattern = patterns[x]
+        print(coord)
+        if pattern is None:
+            drawBox(win, Point(coord[0], coord[1]), Point(coord[0]+100, coord[1]+100), colour)
+        elif pattern == "corner":
+            drawCorner(win, coord, colour)
+        else:
+            drawTile(win, coord, colour)
+
+        win.getMouse()
+
+
 #draws corner patch and returns all objetcs for later use
 def drawCorner(win, point, colour):
     objects = []
@@ -13,6 +29,57 @@ def drawCorner(win, point, colour):
             box = drawBox(win, bottomLeft, topRight, "white")
         objects.append(box)
     return objects
+
+def drawTile(win, point, colour):
+    objects = []
+    for y in range(5):
+        for x in range(5):
+            middle = Point(point[0]+(x*20)+10, point[1]+(y*20)+10)
+            if y%2==0:
+                if x%2==0:
+                    drawArrowsDown(win, middle, colour)
+                else:
+                    drawCircle(win, middle, 10, colour)
+            else:
+                if x%2==0:
+                    drawCircle(win, middle, 10, colour)
+                else:
+                    drawArrowsRight(win, middle, colour)
+    return objects
+
+def drawArrowsRight(win, middle, colour):
+    objects = []
+    topLeft = Point(middle.getX()-10, middle.getY()-10)
+    bottomLeft = Point(middle.getX()-10, middle.getY()+10)
+    Triangle1 = Polygon(middle, topLeft, bottomLeft)
+    Triangle1.setFill(colour)
+    Triangle2 = Triangle1.clone()
+    Triangle2.move(10, 0)
+    Triangle1.draw(win)
+    Triangle2.draw(win)
+    objects.append(Triangle1)
+    objects.append(Triangle2)
+    return objects 
+
+def drawArrowsDown(win, middle, colour):
+    objects = []
+    topLeft = Point(middle.getX()-10, middle.getY()-10)
+    topRight = Point(middle.getX()+10, middle.getY()-10)
+    Triangle1 = Polygon(middle, topLeft, topRight)
+    Triangle1.setFill(colour)
+    Triangle2 = Triangle1.clone()
+    Triangle2.move(0, 10)
+    Triangle1.draw(win)
+    Triangle2.draw(win)
+    objects.append(Triangle1)
+    objects.append(Triangle2)
+    return objects 
+
+def drawCircle(win, point, radius, colour):
+    circle = Circle(point, radius)
+    circle.setFill(colour)
+    circle.draw(win)
+    return circle
 
 #draws box and returns object
 def drawBox(win, point1, point2, colour):
@@ -42,7 +109,7 @@ def getPattern(size):
             elif y%2==0:
                 patterns.append("tile")
             else:
-                patterns.append("null")
+                patterns.append(None)
     return patterns
 
 #returns colours for all tiles in order
@@ -105,8 +172,9 @@ def main():
     #size, choices = getSizeColour(sizes, colours)
     #getColour(5, ["blue", "orange", "red"])
     #getPattern(5)
-    win = createWin("Patchwork", 500)
-    objects = drawCorner(win, [0, 0], "red")
+    win = createWin("Patchwork", 900)
+    #drawTile(win, [0, 0], "red")
+    drawPatchwork(win, getCoord(9), getPattern(9), getColour(9, ["blue", "orange", "red"]))
     win.getMouse()
 
 
